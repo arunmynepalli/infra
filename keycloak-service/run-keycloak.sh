272225c9a748c5570376f621f08723e104170374
arunmynepalli <arunkarthik47@gmail.com>
2021-08-11 11:11:47 +0530

    basic infra files

---
+++ keycloak-service/run-keycloak.sh
@@ -0,0 +1,1 @@
+docker service create --network dev-network -p 8080:8080 --name keycloak-service keycloak-img 
---
+++ nginx/run-nginx.sh
@@ -0,0 +1,1 @@
+docker service create --network dev-network -p 80:80 --name nginx-service nginx-img 
